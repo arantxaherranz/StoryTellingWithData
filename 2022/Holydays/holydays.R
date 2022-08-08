abe041a6c58a80845ad0d63e455f7fb2758475a7
Arantxa Herranz <arantxaherranz@MacBook-Air-de-Arantxa-2.local>
2022-08-08 15:44:18 +0200

    update code and new graph

--- 2022/Holydays/holydays.R
+++ 2022/Holydays/holydays.R
@@ -31,14 +31,15 @@ ggplot(holydays) +
        x ="Day", y = "Month",
        caption = "Author: Arantxa Herranz | Source: qppstudio")
 
+
 #anothr calendar
 install.packages("remotes")
 remotes::install_github("jayjacobs/ggcal")
 library(ggplot2)
 library(ggcal)
 
-mydate <- seq(as.Date("2017-02-01"), as.Date("2017-07-22"), by="1 day")
-myfills <- rnorm(length(mydate))
+mydate <- seq(as.Date("2022-01-01"), as.Date("2022-12-31"), by="1 day")
+myfills <- holydays
 
 print(ggcal(mydate, myfills))
 
@@ -51,6 +52,18 @@ View(number)
 #join month and day in a new column
 holydays$Date <- make_date(year = "2022", month = holydays$Month, day = holydays$Day)
 View(holydays)
+
+ggplot(holydays) + 
+  aes(x = Date, y = fct_rev(Calendar), color = Calendar, na.rm = TRUE) + 
+  geom_point(size = 6)+
+  scale_x_date(date_breaks = "14 day", date_labels = "%d-%m")+
+  scale_y_discrete(limits = rev(levels("Calendar")))+
+  theme_linedraw()+
+  theme(legend.position="none")+
+  labs(title="Bank Holyday by type of Calendar",
+       plot.title = element_text(hjust = 0.5),
+       x ="Day", y = "Calendar",
+       caption = "Author: Arantxa Herranz | Source: qppstudio")
 
 #creating a Day of Year (doy) column
 holydays$doy <- yday(holydays$Date)
